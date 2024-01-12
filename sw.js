df53f8b821c201a6c0149055b3212722a8bbaf0a
Dhruvil Malvania <144580168+Dhruvilllll@users.noreply.github.com>
2024-01-12 13:54:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705038917';
+const cacheName = 'chirpy-1705047713';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
