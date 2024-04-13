05c924ba1f5fc9b3d6c75f69b6377ec0cb753563
Dhruvilllll <dhruvilmalvania@gmail.com>
2024-04-13 21:58:09 +0530

    push

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1713023623';
+const cacheName = 'chirpy-1713025174';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
