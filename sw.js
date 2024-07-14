1281bda0cf10d42f1180625611c57e2c3fc7534c
Dhruvilllll <dhruvilmalvania@gmail.com>
2024-07-14 14:47:12 +0530

    yas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1720533531';
+const cacheName = 'chirpy-1720948563';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
